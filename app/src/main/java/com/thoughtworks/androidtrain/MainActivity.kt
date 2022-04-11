006a4c38debe59b99c5ddd32227a88c46c11b22a
feihong.he <feihong.he@thoughtworks.com>
2022-04-12 00:20:03 +0800

    linear-layout和本地化

--- app/src/main/java/com/thoughtworks/androidtrain/MainActivity.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/MainActivity.kt
@@ -7,7 +7,7 @@ import android.util.Log
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_scroll)
+        setContentView(R.layout.activity_main)
         Log.i("MainActivity","start")
     }
 }
