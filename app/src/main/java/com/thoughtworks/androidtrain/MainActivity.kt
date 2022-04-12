266aa21159b32497a0fdf72193e8ea3f84abd1ab
feihong.he <feihong.he@thoughtworks.com>
2022-04-13 00:31:59 +0800

    完成作业，添加跳转和新的activity

--- app/src/main/java/com/thoughtworks/androidtrain/MainActivity.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.thoughtworks.androidtrain
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scroll)
         Log.i("MainActivity","start")
+    }
+
+    fun goNextActivity(view: View) {
+        val intent = Intent(this, HelloActivity::class.java).apply {}
+        startActivity(intent)
     }
 }
